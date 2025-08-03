2aa98c72481e7e09353ddb1725782d4bbef89fd9
daxferab <daxferab.19@gmail.com>
2025-08-03 20:45:22 +0200

    Finished ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: daxferna <daxferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 15:32:24 by daxferna          #+#    #+#             */
-/*   Updated: 2025/05/18 15:57:58 by daxferna         ###   ########.fr       */
+/*   Updated: 2025/08/03 20:15:55 by daxferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,19 @@
 
 int	main(int argc, char	*argv[])
 {
-	(void)argv;
+	(void) argv;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-		for (int i = 1; argv[i]; i++)
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char) toupper(argv[i][j]);
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (size_t j = 0; j < std::string(argv[i]).length(); j++)
+				std::cout << (char)toupper(argv[i][j]);
+			if (i != argc - 1)
+				std::cout << " ";
+		}
+	}
 	std::cout << std::endl;
+	return (0);
 }
