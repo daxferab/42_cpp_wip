618af7c97b675db42443f31580a2948737969ff9
daxferab <daxferab.19@gmail.com>
2025-09-23 22:37:14 +0200

    Gitignore

---
+++ ex02/Account.cpp
@@ -0,0 +1,66 @@
+#include "Account.hpp"
+
+// [AAAAMMDD_HHMMSS]
+
+int	Account::getNbAccounts( void )
+{
+	return (0);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (0);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (0);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (0);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	
+}
+
+Account::Account( int initial_deposit )
+{
+
+}
+
+Account::~Account( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	return (true);
+}
+
+int		Account::checkAmount( void ) const
+{
+	return (0);
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
+
+void	Account::_displayTimestamp( void )
+{
+	time_t	timestamp;
+
+	timestamp = time(NULL);
+
+}
