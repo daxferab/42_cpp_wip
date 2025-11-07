3be27a129a9817d4ec3417f43c10fea1cc63e3f1
daxferab <daxferab.19@gmail.com>
2025-11-07 21:25:57 +0100

    Fix: leaks cpp01_01

--- CPP_01/ex01/main.cpp
+++ CPP_01/ex01/main.cpp
@@ -9,7 +9,7 @@ int main() {
 	std::cout << "Creating horde of Zombies..." << std::endl << std::endl;
 	for (int i = 0; i < amount; i++)
 		horde[i].announce();
-
+	delete[] horde;
 	std::cout << "--------------------------------------------" << std::endl;
 	return (0);
 }
